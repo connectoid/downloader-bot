3cff2ca1fb9c423e761e20ff31bf6deb9f9e1d72
Alexander Beley <beleysasha@gmail.com>
2024-04-11 21:07:26 +1200

    tg stuff added

--- tools/tools.py
+++ tools/tools.py
@@ -81,7 +81,7 @@ def get_html(url):
             })
             try:
                 driver.get(url)
-                # time.sleep(5)
+                time.sleep(2)
                 html = driver.page_source
                 return html
             except Exception as e:
