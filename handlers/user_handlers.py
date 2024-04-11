565fb63b4253355ef976245c74edfab34c1e7e29
Alexander Beley <beleysasha@gmail.com>
2024-04-11 17:28:05 +1200

    tg stuff added

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -1,17 +1,10 @@
 import os
 from dotenv import load_dotenv
 
-from aiogram import Bot, Dispatcher, F, Router
-from aiogram.exceptions import TelegramBadRequest
+from aiogram import Bot, Router
 from aiogram.filters import Command, CommandStart, Text, StateFilter
 from aiogram.types import (CallbackQuery, Message, ReplyKeyboardRemove, 
                            LabeledPrice, PreCheckoutQuery, ContentType)
-from aiogram.types.message import ContentType
-from aiogram.filters.state import State, StatesGroup
-from aiogram.fsm.context import FSMContext
-from aiogram.fsm.state import default_state
-from aiogram.fsm.storage.memory import MemoryStorage
-from aiogram.enums import ParseMode
 
 load_dotenv()
 BOT_TOKEN = os.getenv('BOT_TOKEN')
